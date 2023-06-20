555302f54029b00739da5a6a31aa989f14765ed8
ryo.hirano <ryo.hirano@monocrea.co.jp>
2023-06-20 22:41:03 +0900

    アプリ起動scriptの改修

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -3,9 +3,7 @@ set -eux
 script_dir_path=$(dirname $(readlink -f $0))
 MVNW=$script_dir_path/../mvnw
 
-$MVNW package
+$MVNW clean package -DskipTests
 cp $script_dir_path/../target/*.jar $script_dir_path/../docker
-# (
-#   cd $script_dir_path/../
-#   docker-compose up -d
-# )
+
+(cd $script_dir_path/../; docker-compose up)
