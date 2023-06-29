b53d0accfb2a20bb217baea0770e8c7c0ba8c017
ryo.hirano <ryo.hirano@monocrea.co.jp>
2023-06-30 08:54:52 +0900

    test data追加

--- docker/flyway/sql/V1.001__initialize.sql
+++ docker/flyway/sql/V1.001__initialize.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION pgcrypto;
 
 CREATE TABLE IF NOT EXISTS member (
-  id uuid PRIMARY KEY DEFAULT public.gen_random_uuid(),
+  id serial PRIMARY KEY,
   first_name varchar(32) NOT NULL,
   last_name varchar(32) NOT NULL
 );
---
+++ docker/flyway/sql/V1.002__InsertTestData.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.member(first_name, last_name) VALUES
+  ('hogeta', 'hogeo'),
+  ('fugata', 'piyoko');
