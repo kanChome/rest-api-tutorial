887f9d6df9cc847b2eff0aa3e0294adb080b3f61
kanChome <study.hellbird@gmail.com>
2023-07-08 12:12:06 +0900

    error handlingの追加

--- src/main/java/com/rest/api/tutorial/services/MemberServiceImpl.java
+++ src/main/java/com/rest/api/tutorial/services/MemberServiceImpl.java
@@ -1,12 +1,15 @@
 package com.rest.api.tutorial.services;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rest.api.tutorial.domains.MemberRequest;
 import com.rest.api.tutorial.domains.MemberResponse;
+import com.rest.api.tutorial.exceptions.ResourceNotFoundException;
 import com.rest.api.tutorial.repositories.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -26,7 +29,7 @@ class MemberServiceImpl implements MemberService {
   @Override
   @Transactional(readOnly = true)
   public MemberResponse findMember(int id) {
-    return memberRepository.findMemberById(id);
+    return findMemberById(id).orElseThrow(ResourceNotFoundException::new);
   }
 
   @Override
@@ -36,14 +39,23 @@ class MemberServiceImpl implements MemberService {
   }
 
   @Override
+  @Transactional
   public void updateMember(int id, MemberRequest memberRequest) {
-    memberRepository.updateMember(id, memberRequest);
+    findMemberById(id).ifPresentOrElse(member -> memberRepository.updateMember(id, memberRequest), () -> {
+      throw new ResourceNotFoundException();
+    });
   }
 
   @Override
   @Transactional
   public void deleteMember(int id) {
-    memberRepository.deleteMember(id);
+    findMemberById(id).ifPresentOrElse(member -> memberRepository.deleteMember(id), () -> {
+      throw new ResourceNotFoundException();
+    });
   }
 
+  private Optional<MemberResponse> findMemberById(int id) {
+    MemberResponse member = memberRepository.findMemberById(id);
+    return Objects.isNull(member) ? Optional.empty() : Optional.of(member);
+  }
 }
